817da7da1bb1f1108308c4741ee1ba35a4cca8b1
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-05-26 21:20:53 +0200

    Add files via upload

---
+++ Quest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest : MonoBehaviour {
+
+    [SerializeField]
+     bool showObjective = false;
+    [SerializeField]
+    Texture objective;
+    [SerializeField]
+    private int collision;
+
+    void Start()
+    {
+        showObjective = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && showObjective == false && collision == 0)
+        showObjective = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            showObjective = false;
+        collision = 1;
+    }
+    void OnGUI()
+    {
+        if (showObjective == true)
+            GUI.DrawTexture(new Rect(Screen.width / 1.5f, Screen.height / 1.4f, 178, 178), objective);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("ShowObj")&& collision == 1)
+        {
+            showObjective = true;
+        }
+         if (Input.GetButtonUp("ShowObj") && collision == 1)
+        {
+            showObjective = false;
+        }
+    }
+    }
