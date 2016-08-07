18f85d60ecd7186631a046d0ff419dfcd956d87c
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-08-07 12:10:40 +0200

    Add files via upload

---
+++ Radiation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radiation : MonoBehaviour
+{
+    public GameObject radiationEffect;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            radiationEffect.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            radiationEffect.SetActive(false);
+        }
+    }
+}
---
+++ TransparencyScript.js
@@ -0,0 +1,10 @@
+﻿var alphaStart : float = 0.0;
+var alphaEnd : float = 1.0;
+var duration : float = 1.0;
+
+function Update() {
+
+    var lerp : float = Mathf.PingPong(Time.time,duration)/duration;
+    GetComponent.<Renderer>().material.color.a = Mathf.Lerp(alphaStart,alphaEnd,lerp);
+
+} 
