1de5ca7c061a7832314cb8d1d839a8d1319850f3
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-09-11 13:57:20 +0200

    Add files via upload

---
+++ Teleport.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject objToTP;
+    public Transform tpLoc;
+    void Start()
+    {
+        ui.SetActive(false);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        ui.SetActive(true);
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            objToTP.transform.position = tpLoc.transform.position;
+        }
+    }
+    void OnTriggerExit()
+    {
+        ui.SetActive(false);
+    }
+}
