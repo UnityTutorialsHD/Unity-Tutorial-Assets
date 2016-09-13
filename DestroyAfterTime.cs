8857f7131f1686ee22c9600abcdb5e26853ed4b5
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-09-13 16:30:58 +0200

    Add files via upload

---
+++ DestroyAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public GameObject effect;
+    public float Time;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("Explode", 5);
+    }
+    void Explode()
+    {
+        Instantiate(effect, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(gameObject);
+    }
+}
