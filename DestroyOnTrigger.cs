4d65643793762bfb072fb8dee8f6e85415cf7fc1
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-09-24 16:28:53 +0200

    Add files via upload

---
+++ DestroyOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    public GameObject objToDestroy;
+    public GameObject effect;
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+            Instantiate(effect, objToDestroy.transform.position, objToDestroy.transform.rotation);
+            Destroy(objToDestroy);
+    }
+}
