3921b25a431440d1503341f5ed5b8b650a7720f8
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-12-04 20:05:26 +0100

    Add files via upload

---
+++ ThrowObject.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 10;
+    bool hasPlayer = false;
+    bool beingCarried = false;
+    public AudioClip[] soundToPlay;
+    private AudioSource audio;
+    public int dmg;
+    private bool touched = false;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 2.5f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+        if (hasPlayer && Input.GetButtonDown("Use"))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            beingCarried = true;
+        }
+        if (beingCarried)
+        {
+            if (touched)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+            if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Rigidbody>().isKinematic = false;
+                    transform.parent = null;
+                    beingCarried = false;
+                    GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+                RandomAudio();
+                }
+                else if (Input.GetMouseButtonDown(1))
+                {
+                GetComponent<Rigidbody>().isKinematic = false;
+                    transform.parent = null;
+                beingCarried = false;
+                }
+            }
+        }
+    void RandomAudio()
+    {
+        if (audio.isPlaying){
+            return;
+                }
+        audio.clip = soundToPlay[Random.Range(0, soundToPlay.Length)];
+        audio.Play();
+
+    }
+   void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+    }
