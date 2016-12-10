295e221ea02818978f063a5f2003c0726bc5822a
UnityTutorialsHD <uroskurandic@yahoo.com>
2016-12-10 18:56:34 +0100

    Add files via upload

---
+++ EnterVehicle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+public class EnterVehicle : MonoBehaviour
+{
+    private bool inVehicle = false;
+    CarUserControl vehicleScript;
+    public GameObject guiObj;
+    GameObject player;
+
+
+    void Start()
+    {
+        vehicleScript = GetComponent<CarUserControl>();
+        player = GameObject.FindWithTag("Player");
+        guiObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && inVehicle == false)
+        {
+            guiObj.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                guiObj.SetActive(false);
+                player.transform.parent = gameObject.transform;              
+                vehicleScript.enabled = true;
+                player.SetActive(false);
+                inVehicle = true;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            guiObj.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if (inVehicle == true && Input.GetKey(KeyCode.F))
+        {
+            vehicleScript.enabled = false;
+            player.SetActive(true);
+            player.transform.parent = null;
+            inVehicle = false;
+        }
+    }
+}
