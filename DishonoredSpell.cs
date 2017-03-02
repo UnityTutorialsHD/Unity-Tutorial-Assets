5a39dd397c1349bed367016a4ad595b9540ff555
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-03-02 23:30:15 +0100

    Add files via upload

---
+++ DishonoredSpell.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DishonoredSpell : MonoBehaviour {
+
+    private RaycastHit lastRaycastHit;
+    public AudioClip audioClip;
+    public float range = 1000;
+
+    private GameObject GetLookedAtObject()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Camera.main.transform.forward;
+        if (Physics.Raycast(origin, direction, out lastRaycastHit, range))
+        {
+            return lastRaycastHit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private void TeleportToLookAt()
+    {
+        transform.position = lastRaycastHit.point + lastRaycastHit.normal * 1.5f;
+        if (audioClip != null)
+            AudioSource.PlayClipAtPoint(audioClip, transform.position);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            if (GetLookedAtObject() != null)
+                TeleportToLookAt();
+    }
+
+
+}
