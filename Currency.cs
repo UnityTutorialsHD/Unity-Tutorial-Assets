92f6662a177bec115b600340a9cbd72e61478935
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-03-04 17:56:20 +0100

    Add files via upload

---
+++ Currency.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour {
+
+    public int gold;
+
+    GameObject currencyUI;
+    void Start()
+    {
+        currencyUI = GameObject.Find("Currency");
+    }
+    void Update () {
+        currencyUI.GetComponent<Text>().text = gold.ToString();
+        if (gold < 0)
+        {
+            gold = 0;
+        }
+	}
+}
---
+++ PickupMoney.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupMoney : MonoBehaviour {
+
+    Currency script;
+
+    public int addAmount;
+
+    void Start()
+    {
+        script = GameObject.FindWithTag("GameController").GetComponent<Currency>();
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "Player")
+        {
+            script.gold += addAmount;
+            Destroy(gameObject);
+        }     
+    }
+}
