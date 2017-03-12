5010412db33a1eb4bb397ba09add74e06449280e
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-03-12 22:26:01 +0100

    Add files via upload

---
+++ SlowDownTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowDownTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Time.timeScale = 0.65f;
+        }
+        else
+            Time.timeScale = 1;
+    }	
+}
---
+++ UnlockAbility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockAbility : MonoBehaviour
+{
+    public bool dishonoredSpell;
+    public bool slowDownTime;
+    // Use this for initialization
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (dishonoredSpell)
+            {
+                GameObject.FindWithTag("Player").GetComponent<DishonoredSpell>().enabled = true;
+                Destroy(gameObject);
+            }
+            if (slowDownTime)
+            {
+                GameObject.FindWithTag("Player").GetComponent<SlowDownTime>().enabled = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
