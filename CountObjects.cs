1d28b7c1e2cfd74683f6d4d516c494fb3e98174b
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-03-24 15:38:44 +0100

    Add files via upload

---
+++ CountObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountObjects : MonoBehaviour {
+    public string nextLevel;
+    public GameObject objToDestroy;
+    GameObject objUI;
+	// Use this for initialization
+	void Start()
+    {
+        objUI = GameObject.Find("ObjectNum");
+    }
+	// Update is called once per frame
+	void Update () {
+        objUI.GetComponent<Text>().text = ObjectsToCollect.objects.ToString();
+        if (ObjectsToCollect.objects == 0)
+        {
+            Application.LoadLevel(nextLevel);
+            //Destroy(objToDestroy);
+            objUI.GetComponent<Text>().text = "All objects collected.";
+        }
+
+	}
+}
---
+++ ObjectsToCollect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsToCollect : MonoBehaviour {
+    public static int objects = 0;
+	// Use this for initialization
+	void Awake () {
+        objects++;
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+            objects--;
+        gameObject.SetActive(false);
+    }
+}
