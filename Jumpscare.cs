1ad4340513c9c55631019442040eace643a8c5ff
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-04-17 14:01:30 +0200

    Add files via upload

---
+++ Jumpscare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscare : MonoBehaviour {
+
+    public GameObject jumpscareObject;
+
+    void Start () {
+
+        jumpscareObject.SetActive(false);
+	}
+	
+	void OnTriggerEnter (Collider player) {
+        if(player.tag == "Player")
+        {
+            jumpscareObject.SetActive(true);
+            StartCoroutine(DestroyObject());
+        }		
+	}
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(jumpscareObject);
+        Destroy(gameObject);
+    }
+}
