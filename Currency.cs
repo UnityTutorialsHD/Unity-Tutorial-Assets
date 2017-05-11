055aee4a895c157bc6648c7c6994080b3e10b311
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-05-11 18:12:28 +0200

    Add files via upload

--- Currency.cs
+++ Currency.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Currency : MonoBehaviour {
+public class Currency : MonoBehaviour
+{
 
     public int gold;
-
     GameObject currencyUI;
+
     void Start()
     {
         currencyUI = GameObject.Find("Currency");
     }
-    void Update () {
+    void Update()
+    {
         currencyUI.GetComponent<Text>().text = gold.ToString();
         if (gold < 0)
         {
             gold = 0;
         }
-	}
+    }
 }
---
+++ PickUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    Animator anim;
+
+    public bool money;
+    public int moneyAmount;
+    Currency moneyScript;
+
+    public bool item;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+        moneyScript = GameObject.FindWithTag("GameController").GetComponent<Currency>();
+
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay(Collider player) {
+        if (player.tag == "Player")
+        {
+            if (money && Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine("PlayAnim");
+            }
+            else if(item && Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine("PlayAnim");
+            }
+        }		
+	}
+
+    IEnumerator PlayAnim()
+    {
+        anim.SetTrigger("pickup");
+        yield return new WaitForSeconds(1);
+        if (money)
+        {
+            moneyScript.gold += moneyAmount;
+            Destroy(gameObject);
+        }
+        else if (item)
+        {
+            //pick up object or add it to your inventory
+        }
+    }
+}
