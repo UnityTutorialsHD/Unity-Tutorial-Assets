e8a48840b2186b9b80d2d622a45f001a2690ada5
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-06-24 18:46:22 +0200

    Add files via upload

---
+++ TurnLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour {
+
+    public GameObject light;
+    private bool on = false;
+
+    // Use this for initialization
+    void OnTriggerStay(Collider plyr) {
+        if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on)
+        {
+            light.SetActive(true);
+            on = true;
+        }
+        else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && on)
+            {
+            light.SetActive(false);
+            on = false;
+        }
+    }
+}
