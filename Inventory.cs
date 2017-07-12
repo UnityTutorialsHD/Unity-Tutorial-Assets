9a3994e2ac7f9a54fac9d97aa7adeba0b3d099f8
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-07-12 19:13:37 +0200

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject invTab;
+
+	// Use this for initialization
+	void Start () {
+        invTab.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            invTab.SetActive(true);
+        }
+        else
+        {
+            invTab.SetActive(false);
+        }
+	}
+}
