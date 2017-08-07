516538e035a89fbb3e3beaa03a4a1b45edfecd09
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-08-07 02:41:50 +0200

    Add files via upload

---
+++ UseInventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class UseInventory : MonoBehaviour
+{
+
+    public bool stats;
+    public bool item;
+
+    public int tValue;
+    public int hValue;
+
+    GameObject inv;
+    GameObject player;
+
+
+    void Start()
+    {
+        inv = GameObject.FindWithTag("GameController");
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void UseItem()
+    {
+        if (stats)
+        {
+            player.GetComponent<ThirdPersonCharacter>().thirst += tValue;
+            player.GetComponent<ThirdPersonCharacter>().hunger += hValue;
+            Destroy(gameObject);
+        }
+
+        if (item)
+        {
+            //use item
+        }
+    }
+}
