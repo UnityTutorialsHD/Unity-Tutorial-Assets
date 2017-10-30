4ce0f88e7654f121c699bbba17dc77cfdee6b670
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-10-30 18:58:38 +0100

    Add files via upload

---
+++ Territory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Territory : MonoBehaviour {
+
+    public string territoryName;
+    public GameObject territoryUI;
+
+	// Use this for initialization
+	void Start () {
+
+        territoryUI.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider player) {
+		if(player.gameObject.tag == "Player")
+        {
+            territoryUI.SetActive(true);
+            territoryUI.GetComponent<Text>().text = territoryName;
+        }
+	}
+    void OnTriggerExit(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            territoryUI.SetActive(false);
+        }
+    }
+}
