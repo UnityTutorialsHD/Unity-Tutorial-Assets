278e83b92f37b91bb347de0289f6fbd443c39b7d
UnityTutorialsHD <uroskurandic@yahoo.com>
2017-11-23 12:57:07 +0100

    Add files via upload

---
+++ ComboHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboHit : MonoBehaviour
+{
+    Animator mAnim;
+
+    void Start()
+    {
+        mAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mAnim.SetTrigger("punch");
+        }
+    }
+}
---
+++ ShowUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour {
+
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+	// Update is called once per frame
+	void OnTriggerEnter (Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+	}
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+    
+}
