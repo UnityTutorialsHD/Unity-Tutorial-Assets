5064a68e9c7666dde965ef19e43b99d20b78618d
UnityTutorialsHD <uroskurandic@yahoo.com>
2018-01-07 16:04:27 +0100

    Add files via upload

---
+++ PlayVideo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+	// Use this for initialization
+	void Start () {
+        videoPlayer.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider player) {
+
+        if (player.gameObject.tag == "Player")
+        {
+            videoPlayer.SetActive(true);
+            Destroy(videoPlayer, timeToStop);
+        }
+	}
+}
