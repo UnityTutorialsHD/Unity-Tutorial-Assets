ef866725b38877030b3e005f2c434f2b63c13615
UnityTutorialsHD <uroskurandic@yahoo.com>
2018-02-26 16:50:09 +0100

    Add files via upload

---
+++ ChangeCheckpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCheckpoint : MonoBehaviour
+{
+    public GameObject checkpoint;
+    // Use this for initialization
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+            Destroy(checkpoint);
+        Destroy(gameObject);
+    }
+}
---
+++ Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public Transform checkpoint;
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider plyr) {
+		if(plyr.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+            player.transform.rotation = checkpoint.rotation;
+        }
+	}
+}
