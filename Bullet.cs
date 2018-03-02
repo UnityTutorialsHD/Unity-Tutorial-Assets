c6308f161eb77b53b48d0f2fbe9e0e14cb9962fb
UnityTutorialsHD <uroskurandic@yahoo.com>
2018-03-02 19:20:06 +0100

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5);
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter () {
+        Destroy(gameObject);
+	}
+}
---
+++ Gun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public Transform bulletSpawn;
+    public Rigidbody bullet;
+    public float bulletSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody bulletRigidbody;
+            bulletRigidbody = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation) as Rigidbody;
+            bulletRigidbody.AddForce(bulletSpawn.forward * bulletSpeed);
+        }     
+	}
+}
