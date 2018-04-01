ac94a9ecfbd26f56b872e3025030cbac8d72a954
UnityTutorialsHD <uroskurandic@yahoo.com>
2018-04-01 16:39:22 +0200

    Add files via upload

---
+++ Waypoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float WPradius = 1;
+	
+	void Update () {
+		if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current = Random.Range(0,waypoints.Length);
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+
+    }
+}
