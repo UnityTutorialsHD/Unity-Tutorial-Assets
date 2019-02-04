da543304eb06652cc5b7606ed63dea578c1f183a
UnityTutorialsHD <uroskurandic@yahoo.com>
2019-02-04 10:53:09 +0100

    Add files via upload

---
+++ OnTriggerLoadLevelV2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTriggerLoadLevelV2 : MonoBehaviour
+{
+
+    public GameObject enterText;
+    public string levelToLoad;
+
+    void Start()
+    {
+        enterText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            enterText.SetActive(true);
+            if (Input.GetButtonDown("Use"))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+    void OnTriggerExit(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            enterText.SetActive(false);
+        }
+    }
+}
